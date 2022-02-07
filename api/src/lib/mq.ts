39a27294587eac186f4f613ce1eafc6085a30dac
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-02-07 21:43:08 +0530

    feat(api): :bricks: Added initial message queue functionality

---
+++ api/src/lib/mq.ts
@@ -0,0 +1,35 @@
+import { Channel, connect, Connection } from 'amqplib'
+
+class MessageQueue {
+  private static connection: undefined | Connection = undefined
+  private static channel: undefined | Channel
+  private connected = () => MessageQueue.connection !== undefined
+
+  constructor() {
+    this.initConnection()
+  }
+
+  private async initConnection() {
+    if (this.connected()) {
+      return true
+    }
+    MessageQueue.connection = await connect(process.env.AMPQ_URL)
+    MessageQueue.channel = await MessageQueue.connection.createChannel()
+    return true
+  }
+
+  async sendToChannel(data) {
+    const queueName = 'metacritic'
+    await MessageQueue.channel.assertQueue(queueName, { durable: true })
+    MessageQueue.channel.sendToQueue(
+      queueName,
+      Buffer.from(`Hello World! ${JSON.stringify(data)}`),
+      {
+        deliveryMode: true,
+      }
+    )
+    console.log(`Sent 'Hello World!' to ${queueName} ${JSON.stringify(data)}`)
+  }
+}
+
+export const mq = new MessageQueue()
--- api/src/services/tasks/tasks.ts
+++ api/src/services/tasks/tasks.ts
@@ -1,12 +1,15 @@
 import { db } from 'src/lib/db'
 import { Prisma } from '@prisma/client'
+import { mq } from 'src/lib/mq'
 
 export const tasks = () => {
   return db.task.findMany()
 }
 
 export const createTask = ({ input }: { input: Prisma.TaskCreateInput }) => {
-  return db.task.create({
+  const task = db.task.create({
     data: input,
   })
+  mq.sendToChannel(input.data)
+  return task
 }
